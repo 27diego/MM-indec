55318b3335d2eb4871e2f532d357cd00944b13e3
27diego <divegamaravilla@csumb.edu>
2020-03-27 22:25:37 -0700

    class?

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -12,6 +12,12 @@ export const Dashboard = () => {
   const [document, setDocument] = useState("");
   const [PDFModal, setPDFModal] = useState(true);
 
+  // state = {
+  //   department: "",
+  //   document: "",
+  //   PDFModal: false
+  // }
+
   return (
     <div className="Container--Dashboard">
       <div className="container--MenuPannel">
